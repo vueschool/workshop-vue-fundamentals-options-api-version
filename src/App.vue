f01bd535ecd6d8cfd742994ec9ae31cb0c440485
Daniel Kelly <me@danielkelly.io>
2023-06-08 15:13:43 -0500

    exercise 8 solution

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script>
-export default {};
+import AppPlanet from "./components/AppPlanet.vue";
+export default {
+  components: { AppPlanet },
+  data() {
+    return {
+      planets: [
+        { name: "Mercury", visits: 2 },
+        { name: "Venus", visits: 0 },
+        { name: "Jupiter", visits: 5 },
+        { name: "Mars", visits: 1 },
+      ],
+    };
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <ul>
+    <AppPlanet
+      v-for="planet in planets"
+      :key="planet.name"
+      :name="planet.name"
+      :visits="planet.visits"
+    ></AppPlanet>
+  </ul>
+</template>
---
+++ src/components/AppPlanet.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    visits: { type: Number, required: true },
+  },
+  computed: {
+    showIcon() {
+      return this.visits > 0;
+    },
+  },
+};
+</script>
+<template>
+  <li>
+    <span>
+      <strong>Planet:</strong>
+      {{ name }} Visted {{ visits }} time(s)
+    </span>
+    <span v-if="showIcon">🚀</span>
+  </li>
+</template>
+
+<style scoped>
+li {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
