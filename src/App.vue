00f6e26b9dde1563ed8166974e1b2d14c7aab6d8
Daniel Kelly <me@danielkelly.io>
2023-06-12 15:40:56 -0500

    starting code for exercise 10

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <script>
-import AppPlanet from "./components/AppPlanet.vue";
 export default {
   components: { AppPlanet },
   data() {
     return {
-      planets: [
-        { name: "Mercury", visits: 2 },
-        { name: "Venus", visits: 0 },
-        { name: "Jupiter", visits: 5 },
-        { name: "Mars", visits: 1 },
-      ],
+      usernames: ["yyx990803", "antfu", "danielkellyio"],
     };
   },
 };
 </script>
 
 <template>
-  <ul>
-    <AppPlanet
-      v-for="planet in planets"
-      :key="planet.name"
-      :name="planet.name"
-      :visits="planet.visits"
-      @visited="planet.visits = $event"
-    ></AppPlanet>
-  </ul>
+  <div></div>
 </template>
